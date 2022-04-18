94ac5d6c49049f22f51c0e509483c963e35c6b7f
Ari Archer <ari.web.xyz@gmail.com>
2022-04-18 18:35:59 +0300

    update @ Mon 18 Apr 18:35:59 EEST 2022
    Signed-off-by: Ari Archer <ari.web.xyz@gmail.com>

--- src/tty/tty.c
+++ src/tty/tty.c
@@ -6,11 +6,12 @@ int main(int argc, char *argv[]) {
     const char *tty_dev_name     = ttyname(STDIN_FILENO);
     const unsigned char do_print = strcmp(argc > 1 ? argv[1] : "", "-s");
 
-    if (!tty_dev_name)
-        if (do_print) {
+    if (!tty_dev_name) {
+        if (do_print)
             perror("ttyname()");
-            return 1;
-        }
+
+        return 1;
+    }
 
     if (do_print)
         puts(tty_dev_name);
