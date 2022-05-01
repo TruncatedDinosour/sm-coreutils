b937b5ef20811e5d8dcb56bc559cc0662df2d3c9
Ari Archer <ari.web.xyz@gmail.com>
2022-05-01 21:36:26 +0300

    Fix hardening
    Signed-off-by: Ari Archer <ari.web.xyz@gmail.com>

--- builders/bash/hardened.bash
+++ builders/bash/hardened.bash
@@ -3,7 +3,8 @@
 export CFLAGS="-fstack-protector-strong -fstack-protector -fPIE -pie \
     -U_FORTIFY_SOURCE -D_FORTIFY_SOURCE=2 -O2 \
     -Wno-unused-result -Wno-unused-command-line-argument \
-    -D_GLIBC_ASSERTIONS -fasynchronous-unwind-tables -fexceptions \
+    -U_GLIBCXX_ASSERTIONS -D_GLIBCXX_ASSERTIONS -U_GLIBC_ASSERTIONS -D_GLIBC_ASSERTIONS \
+    -fasynchronous-unwind-tables -fexceptions \
     -fpie -fpic -fstack-clash-protection \
     -fstack-protector-all -grecord-gcc-switches -fcf-protection -Werror=format-security \
     -Wl,-z,now"
